461e988c7764e58f969aa907c5b10a4e1a11f84e
Thanawat Gulati <work.t6tg@gmail.com>
2021-08-14 17:52:35 +0700

    feat: logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PrivateRoute from "./components/PrivateRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Search from "./pages/Search";
@@ -8,18 +7,21 @@ import Detail from "./pages/Detail";
 
 export default function App() {
   return (
-  <>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <PrivateRoute exact path="/search" component={Search} />
-        <PrivateRoute exact path="/detail/:id" component={Detail} />
-      </Switch>
-    </Router>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@400;700&display=swap" rel="stylesheet" />
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/detail/:id" component={Detail} />
+        </Switch>
+      </Router>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Prompt:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
     </>
   );
 }
